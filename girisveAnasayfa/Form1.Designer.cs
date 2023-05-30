5f3adec8f82e3c6013b036918deff4091daa3f58
yaseminyucel <yaseminberranuran@gmail.com>
2023-05-30 15:58:36 +0300

    giris ve anasayfa
    giris ve anasayfa ekranı

---
+++ girisveAnasayfa/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace girisveAnasayfa
+{
+    partial class FrmGiris
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmGiris));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.bttnGiris = new System.Windows.Forms.Button();
+            this.rbttnDiger = new System.Windows.Forms.RadioButton();
+            this.rbttnfakulteS = new System.Windows.Forms.RadioButton();
+            this.textGirisSifre = new System.Windows.Forms.TextBox();
+            this.textGirisMail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(305, 34);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(433, 393);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.pictureBox5);
+            this.panel1.Controls.Add(this.pictureBox4);
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.bttnGiris);
+            this.panel1.Controls.Add(this.rbttnDiger);
+            this.panel1.Controls.Add(this.rbttnfakulteS);
+            this.panel1.Controls.Add(this.textGirisSifre);
+            this.panel1.Controls.Add(this.textGirisMail);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(12, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(296, 393);
+            this.panel1.TabIndex = 1;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // bttnGiris
+            // 
+            this.bttnGiris.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.bttnGiris.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.bttnGiris.ForeColor = System.Drawing.Color.White;
+            this.bttnGiris.Location = new System.Drawing.Point(25, 341);
+            this.bttnGiris.Name = "bttnGiris";
+            this.bttnGiris.Size = new System.Drawing.Size(247, 36);
+            this.bttnGiris.TabIndex = 8;
+            this.bttnGiris.Text = "GİRİŞ YAP";
+            this.bttnGiris.UseVisualStyleBackColor = false;
+            this.bttnGiris.Click += new System.EventHandler(this.bttnGiris_Click);
+            // 
+            // rbttnDiger
+            // 
+            this.rbttnDiger.AutoSize = true;
+            this.rbttnDiger.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbttnDiger.Location = new System.Drawing.Point(197, 300);
+            this.rbttnDiger.Name = "rbttnDiger";
+            this.rbttnDiger.Size = new System.Drawing.Size(75, 19);
+            this.rbttnDiger.TabIndex = 7;
+            this.rbttnDiger.TabStop = true;
+            this.rbttnDiger.Text = "Personel";
+            this.rbttnDiger.UseVisualStyleBackColor = true;
+            // 
+            // rbttnfakulteS
+            // 
+            this.rbttnfakulteS.AutoSize = true;
+            this.rbttnfakulteS.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rbttnfakulteS.Location = new System.Drawing.Point(72, 300);
+            this.rbttnfakulteS.Name = "rbttnfakulteS";
+            this.rbttnfakulteS.Size = new System.Drawing.Size(117, 19);
+            this.rbttnfakulteS.TabIndex = 6;
+            this.rbttnfakulteS.TabStop = true;
+            this.rbttnfakulteS.Text = "Fakülte Sekreteri";
+            this.rbttnfakulteS.UseVisualStyleBackColor = true;
+            // 
+            // textGirisSifre
+            // 
+            this.textGirisSifre.Location = new System.Drawing.Point(75, 236);
+            this.textGirisSifre.Name = "textGirisSifre";
+            this.textGirisSifre.Size = new System.Drawing.Size(197, 20);
+            this.textGirisSifre.TabIndex = 5;
+            // 
+            // textGirisMail
+            // 
+            this.textGirisMail.Location = new System.Drawing.Point(75, 172);
+            this.textGirisMail.Name = "textGirisMail";
+            this.textGirisMail.Size = new System.Drawing.Size(197, 20);
+            this.textGirisMail.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(72, 282);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Kullanıcı Tipi Seçiniz:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(72, 219);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kullanıcı Şifre:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(72, 154);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kullanıcı Mail:";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(86, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(146, 110);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(25, 159);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(41, 33);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.Location = new System.Drawing.Point(25, 223);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(41, 33);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(25, 286);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(41, 33);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 11;
+            this.pictureBox5.TabStop = false;
+            // 
+            // FrmGiris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(750, 478);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmGiris";
+            this.Text = "giris";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button bttnGiris;
+        private System.Windows.Forms.RadioButton rbttnDiger;
+        private System.Windows.Forms.RadioButton rbttnfakulteS;
+        private System.Windows.Forms.TextBox textGirisSifre;
+        private System.Windows.Forms.TextBox textGirisMail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ girisveAnasayfa/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+
+namespace girisveAnasayfa
+{
+    public partial class FrmGiris : Form
+    {
+        public FrmGiris()
+        {
+            InitializeComponent();
+        }
+        MySqlConnection connection  = new MySqlConnection("Server=172.21.54.3; Database=Grup5-2023; Uid=Grup5-2023; Pwd=NyP:974.cc");
+
+        private void bttnGiris_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection.Open();
+                string sql = "SELECT * FROM kullanicilar WHERE kullanici_mail=@kmail AND kullanici_sifre=@ksifre";
+                MySqlParameter params1 = new MySqlParameter("kmail", textGirisMail.Text);
+                MySqlParameter params2 = new MySqlParameter("ksifre", textGirisSifre.Text);
+                MySqlCommand command = new MySqlCommand(sql, connection);
+                command.Parameters.Add(params1);
+                command.Parameters.Add(params2);
+                DataTable dt = new DataTable();
+                MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+                adapter.Fill(dt);
+                if(dt.Rows.Count > 0 )
+                {
+                    FrmAnasayfa fr = new FrmAnasayfa();
+                    fr.Show();
+                }
+
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Hatalı Giriş");
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
