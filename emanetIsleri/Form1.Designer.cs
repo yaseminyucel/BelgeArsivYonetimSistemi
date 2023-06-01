691739b72b8dc4bdf8c30b366d6a03122f2e2da2
yildirimyasemin <yldrmyasemin14@gmail.com>
2023-06-01 23:12:01 +0300

    emanetIsleri

---
+++ emanetIsleri/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace emanetIsleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabCtrlEmanetIsleri = new System.Windows.Forms.TabControl();
+            this.tabPageEvrakEmanet = new System.Windows.Forms.TabPage();
+            this.tabPageEmanetGecmis = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tabCtrlEmanetIsleri.SuspendLayout();
+            this.tabPageEvrakEmanet.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabCtrlEmanetIsleri
+            // 
+            this.tabCtrlEmanetIsleri.Controls.Add(this.tabPageEvrakEmanet);
+            this.tabCtrlEmanetIsleri.Controls.Add(this.tabPageEmanetGecmis);
+            this.tabCtrlEmanetIsleri.Location = new System.Drawing.Point(12, 12);
+            this.tabCtrlEmanetIsleri.Name = "tabCtrlEmanetIsleri";
+            this.tabCtrlEmanetIsleri.SelectedIndex = 0;
+            this.tabCtrlEmanetIsleri.Size = new System.Drawing.Size(865, 506);
+            this.tabCtrlEmanetIsleri.TabIndex = 0;
+            // 
+            // tabPageEvrakEmanet
+            // 
+            this.tabPageEvrakEmanet.Controls.Add(this.dataGridView1);
+            this.tabPageEvrakEmanet.Controls.Add(this.textBox5);
+            this.tabPageEvrakEmanet.Controls.Add(this.textBox4);
+            this.tabPageEvrakEmanet.Controls.Add(this.textBox3);
+            this.tabPageEvrakEmanet.Controls.Add(this.textBox2);
+            this.tabPageEvrakEmanet.Controls.Add(this.textBox1);
+            this.tabPageEvrakEmanet.Controls.Add(this.label5);
+            this.tabPageEvrakEmanet.Controls.Add(this.label4);
+            this.tabPageEvrakEmanet.Controls.Add(this.label3);
+            this.tabPageEvrakEmanet.Controls.Add(this.label2);
+            this.tabPageEvrakEmanet.Controls.Add(this.label1);
+            this.tabPageEvrakEmanet.Location = new System.Drawing.Point(4, 25);
+            this.tabPageEvrakEmanet.Name = "tabPageEvrakEmanet";
+            this.tabPageEvrakEmanet.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageEvrakEmanet.Size = new System.Drawing.Size(857, 477);
+            this.tabPageEvrakEmanet.TabIndex = 0;
+            this.tabPageEvrakEmanet.Text = "Evrak Emanet Ver";
+            this.tabPageEvrakEmanet.UseVisualStyleBackColor = true;
+            // 
+            // tabPageEmanetGecmis
+            // 
+            this.tabPageEmanetGecmis.Location = new System.Drawing.Point(4, 25);
+            this.tabPageEmanetGecmis.Name = "tabPageEmanetGecmis";
+            this.tabPageEmanetGecmis.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPageEmanetGecmis.Size = new System.Drawing.Size(857, 477);
+            this.tabPageEmanetGecmis.TabIndex = 1;
+            this.tabPageEmanetGecmis.Text = "Emanet Geçmişi Görüntüle";
+            this.tabPageEmanetGecmis.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(99, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Evrak Bilgileri";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(129, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(173, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(327, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(327, 51);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "label5";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(416, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(416, 100);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(416, 158);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(416, 212);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(416, 262);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 13;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(47, 299);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(768, 150);
+            this.dataGridView1.TabIndex = 14;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 513);
+            this.Controls.Add(this.tabCtrlEmanetIsleri);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabCtrlEmanetIsleri.ResumeLayout(false);
+            this.tabPageEvrakEmanet.ResumeLayout(false);
+            this.tabPageEvrakEmanet.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabCtrlEmanetIsleri;
+        private System.Windows.Forms.TabPage tabPageEvrakEmanet;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPageEmanetGecmis;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
